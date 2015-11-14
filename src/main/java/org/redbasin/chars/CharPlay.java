87fef3ccb902e880566fe1459264bf2ef4527e43
Tanisha Joshi <tanishajoshi.guitar@gmail.com>
2015-11-14 12:33:28 -0800

    compute pi

---
+++ src/main/java/org/redbasin/chars/CharPlay.java
@@ -0,0 +1,16 @@
+
+
+public class CharPlay {
+
+   public static void main(String[] args) {
+
+     char c = 6;
+     System.out.println(c);
+
+     for (char i = 65; i < 65+52; i++) {
+        if (i == 'a')
+           System.out.println((int)i);
+     }
+
+   }
+}
