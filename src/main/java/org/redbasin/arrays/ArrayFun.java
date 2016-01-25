f6cfa54f2ad34319b330ba4da2963bdeb8c74ab3
Tanisha Joshi <tanishajoshi.guitar@gmail.com>
2016-01-24 16:47:39 -0800

    array 3 dimensional

---
+++ src/main/java/org/redbasin/arrays/ArrayFun.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) Tanisha - All Rights Reserved.
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential.
+ */
+
+package org.redbasin.arrays;
+
+import java.util.Scanner;
+
+/**
+ * @author tanisha on 1/17/16.
+ */
+public class ArrayFun {
+
+  // 1 dim array that stores weekdays
+  static String[] weekdays = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+
+  public static String getWeekDay(int weekdayNo) {
+    return weekdays[weekdayNo];
+  }
+
+  public static void main(String[] args) {
+    for (int i = 0; i < weekdays.length; i++) {
+      System.out.println(getWeekDay(i));
+    }
+
+  }
+}
