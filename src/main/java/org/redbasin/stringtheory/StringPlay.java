849075ace53ccbd4928431a941965ef2110f6ff8
Tanisha Joshi <tanishajoshi.guitar@gmail.com>
2016-01-24 16:48:52 -0800

    array 3 dimensional

---
+++ src/main/java/org/redbasin/stringtheory/StringPlay.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) Tanisha - All Rights Reserved.
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential.
+ */
+
+package org.redbasin.stringtheory;
+
+/**
+ * @author tanisha on 1/17/16.
+ */
+public class StringPlay {
+
+  public static void main(String[] args) {
+
+    int x = 2, y = 3;
+
+    // this prints as a string concatenation
+    System.out.println("x + y" + " = " + x + y);
+
+    // this does actual numerical calculation
+    System.out.println("x + y" + " = " + (x + y));
+  }
+}
