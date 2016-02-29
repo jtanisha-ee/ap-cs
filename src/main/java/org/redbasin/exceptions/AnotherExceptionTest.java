3c093822ceb94cf8e74d99bfe5dafbef78c5c32d
Tanisha Joshi <tanishajoshi.guitar@gmail.com>
2016-02-28 19:34:35 -0800

    array 3 dimensional

---
+++ src/main/java/org/redbasin/exceptions/AnotherExceptionTest.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) Tanisha - All Rights Reserved.
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential.
+ */
+
+package org.redbasin.exceptions;
+
+/**
+ * @author tanisha on 2/28/16.
+ */
+public class AnotherExceptionTest {
+
+  public static void main(String[] args) {
+    int[] arrayOfIntegers = new int[10];
+    System.out.println(arrayOfIntegers[9]);
+  }
+}
