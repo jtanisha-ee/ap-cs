8669dec37c0ec52476259c37e516c4c74db50897
jonam <joshi.manoj@gmail.com>
2016-09-25 11:31:43 -0700

    Added a bunch of algorithms

---
+++ src/main/java/org/redbasin/statictheory/StaticPlay.java
@@ -0,0 +1,23 @@
+package org.redbasin.statictheory;
+
+/**
+ * Created by manojjoshi on 9/18/16.
+ */
+public class StaticPlay {
+
+    public static void main(String[] args) {
+        Circle.PI = 3.14159;
+        Circle c1 = new Circle();
+        Circle c2 = new Circle();
+        System.out.println("PI = " + c1.PI);
+        System.out.println("PI = " + c2.PI);
+        Circle.PI = 3.14;
+        System.out.println("PI = " + c1.PI);
+        System.out.println("PI = " + c2.PI);
+    }
+}
+
+class Circle {
+    static double PI;  // share across circles
+    double radius;
+}
